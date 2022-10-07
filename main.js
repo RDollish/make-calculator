cb5946644004747c234e147d359e1af3ec527087
Renee Doll <renee@reneedoll.com>
2022-10-07 10:09:44 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const add = (firstNumber, secondNumber) => {
+    const sum = firstNumber + secondNumber
+    return sum
+}
+const substrac = (firstNumber, secondNumber) => {
+    const sum = firstNumber - secondNumber
+    return sum
+}
+const multiply = (firstNumber, secondNumber) => {
+    const sum = firstNumber * secondNumber
+    return sum
+}
+const divide = (firstNumber, secondNumber) => {
+    const sum = firstNumber / secondNumber
+    return sum
+}
+const substract = (firstNumber) => {
+    const sum = firstNumber * firstNumber
+    return sum
+}
+console.log(divide(60,4))
+console.log(divide(60,2))
+console.log(multiply(14,.25))
+console.log(multiply(30,.75))
+console.log(add(3.5,22.5))
